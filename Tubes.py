f703888111c4661cf5c62681e82670d13a26b887
idanz19-meet <idanz19@meet.mit.edu>
2017-08-07 11:33:40 +0200

    Stopping here

--- Tubes.py
+++ Tubes.py
@@ -3,14 +3,24 @@ import turtle
 turtle.shape("square")
 #Generate stamps by turtle positions to make the tubes
 #random length 1-10
-turtle.pu()
-turtle.goto(300,200)
-turtle.rt(90)
-for i in range(1,5):
-    turtle.stamp()
-    turtle.fd(20)
-turtle.forward(60)
-for x in range(1,5):
-    turtle.stamp()
-    turtle.fd(20)
+LEFT="Left"
+LEFT_D=1
+direction=LEFT_D
+def move_tubes():
+    global direction
+    direction=LEFT_D
+##turtle.pu()
+##turtle.goto(300,100)
+##turtle.stamp()
+##turtle.rt(90)
+##turtle.fd(100)
+##turtle.stamp()
+turtle.onkeypress(move_tubes,LEFT_D)
+turtle.listen()
+    #turtle.ontimer(move_tubes,100)
+move_tubes()
 
+
+##tube1=turtle.clone()
+##turtle.register_shape("tube1.gif")
+##tube1.shape("tube1.gif")
