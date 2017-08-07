e1f15f457cfa43fa5651dae0b28b3d8984181c0a
idanz19-meet <idanz19@meet.mit.edu>
2017-08-07 15:23:19 +0200

    Stopping here

--- bird2.py
+++ bird2.py
@@ -1,25 +1,37 @@
 import turtle
+import time
 turtle.hideturtle()
-turtle.register_shape("another_bird.gif")
+turtle.register_shape("newnew_bird.gif")
 
 birdy = turtle.clone()
-birdy.shape("another_bird.gif")
+birdy.shape("newnew_bird.gif")
 birdy.penup()
 birdy.showturtle()
 
 UP_ARROW = "Up"
+
+draw = turtle.clone()
+draw.hideturtle()
 
 def grav():
     my_pos = birdy.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
     birdy.goto(x_pos, (y_pos - 10))
-    turtle.ontimer(grav, 100)
-    
+    turtle.ontimer(grav, 50)
+    if birdy.pos()[1] > 410:
+        draw.write("GAME OVER!", font=("Arial", 30, "normal"),align="center")
+        time.sleep(2)
+        quit()
+    if birdy.pos()[1] < -410:
+        draw.write("GAME OVER!", font=("Arial", 30, "normal"),align="center")
+        time.sleep(2)
+        quit()
+
 grav()
 
 def up():
-    for i in range(10):
+    for i in range(7):
         my_pos = birdy.pos()
         x_pos = my_pos[0]
         y_pos = my_pos[1]
@@ -28,4 +40,34 @@ def up():
 turtle.onkeypress(up, UP_ARROW)
 turtle.listen()
 
+
+SIZE_X=1200
+SIZE_Y=900
+turtle.setup(SIZE_X,SIZE_Y)
+turtle.bgcolor("light blue")
+
+global food_stamp,food_pos
+if birdy.pos() in food_pos:
+    global score
+    food_ind=food_pos.index(birdy.pos())
+    food.clearstamp(food_stamp[food_ind])
+    food_stamp.pop(food_ind)
+    food_pos.pop(food_ind)
+    score+=1
+    scorePen = turtle.clone()
+    scorePen.color("light blue")
+    scorePen.shape("square")
+    scorePen.hideturtle()
+    scorePen.penup()
+    scorePen.goto(230, 192)
+    scorePen.stamp()
+    scorePen.goto(230 + SQUARE_SIZE, 192)
+    scorePen.stamp()
+    scorePen.goto(125,175)
+    scorePen.color("black")
+    scorePen.write("Score: " + str(score), font = ("Ariel", 20, "normal"))
+    print("you have eaten the food and scored one point!!!")
+    scorePen.color("white")
+    scorePen.circle(30)
+    make_food()
 
